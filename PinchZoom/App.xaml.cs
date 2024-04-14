f422805d1f7a79aae318353621069165fae773cd
graysonL <liam.m.a.jones@gmail.com>
2024-04-14 16:06:59 +0800

    Add project files.

---
+++ PinchZoom/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PinchZoom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
